e0bc22d94959f0f42344040c69526eff633a08a3
katinas15 <laimonas.janutenas@gmail.com>
2020-09-22 23:57:42 +0300

    preview 1

--- netherwart.js
+++ netherwart.js
@@ -1,32 +1,49 @@
 var robot = require("robotjs");
 
-let x = 5
+let x = 50
+let y = 50
 
-let sleepClick = 100
+let sleepClick = 20
 let moveLengthTime = 290
 
 
 function start(){
     while(true){
         sleep(1000)
-
-        for(let i = 0; i<x; i++){
+        for(let i = 0; i<y; i++){
+            for(let j = 0; j<x; j++){
+                if(!(j == 0 && i == 0)) pickAndPlant()
+                move('w')
+            }
+    
             pickAndPlant()
-            move('w')
+            move('a')
+    
+    
+            for(let j = 0; j<x; j++){
+                pickAndPlant()
+                move('s')
+            }
+            move('s')
+            move('s')
+    
+            pickAndPlant()
+            move('a')
         }
-
-        pickAndPlant()
-        move('a')
+        move('d')
 
 
-        for(let i = 0; i<x; i++){
-            pickAndPlant()
+        for(let i = 0; i<y+10; i++){
+            move('d')
             move('s')
         }
+        sleep(1000)
+        robot.mouseClick("right")
+        dropInventory()
 
-        pickAndPlant()
-        move('a')
-     
+        robot.keyToggle('escape', 'down')
+        sleep(10)
+        robot.keyToggle('escape', 'up')
     }
 }
 
@@ -41,6 +58,34 @@ function move(letter){
     robot.keyToggle(letter, 'down')
     sleep(moveLengthTime)
     robot.keyToggle(letter, 'up')
+}
+
+let offset = 40
+let posx = 815
+let posy = 600
+const rows = 4
+const cols = 9
+
+function dropInventory(){
+    let invPos = []
+    let loc
+    for(let i = 0;i<rows;i++){
+        for(let o = 0;o<cols;o++){
+            loc = {y: posy + offset*i, x: posx + offset*o}
+            console.log(loc)
+            invPos.push(loc)
+        }
+    }
+
+
+    robot.keyToggle('shift', 'down')
+    for(let i in invPos){
+        let pos = invPos[i]
+        sleep(100)
+        robot.moveMouse(pos.x, pos.y);
+        robot.mouseClick()
+    }
+    robot.keyToggle('shift', 'up')
 }
 
 
